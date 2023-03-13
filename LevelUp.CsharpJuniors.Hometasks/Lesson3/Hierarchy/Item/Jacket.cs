8773ff4122803aa3f9386ae316ed68e1cd93ed1c
potemochkin <dr.potemkin@yandex.ru>
2023-03-13 21:49:55 +0300

    Выполнение 3 домашнего задания

---
+++ LevelUp.CsharpJuniors.Hometasks/Lesson3/Hierarchy/Item/Jacket.cs
@@ -0,0 +1,22 @@
+﻿using Lesson3.Interfaces;
+
+namespace Lesson3.Hierarchy.Item
+{
+    internal sealed class Jacket : Wear, Information
+    {
+        public Jacket(string name, string description, int idNumber, int nomenclature, int price, int size, string season) :
+            base(name, description, idNumber, nomenclature)
+        {
+            Price = price;
+            Size = size;
+            Season = season;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Название одежды: {Name}, размер: {Size}, сезон: {Season}, стоимость: {Price} у.е.");
+        }
+        public override int Price { get; set; }
+        public override int Size { get; set; }
+        public override string? Season { get; set; }
+    }
+}
